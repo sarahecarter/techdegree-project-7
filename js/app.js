c3851ec533691f648cffb07384c56f675b563afb
Sarah Carter <scarter3@conncoll.edu>
2020-05-22 21:10:44 -0400

    This commit will start the css styling of the page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-///// Alert Banner /////
+////////////// Alert Banner ///////////////
 
 // Variables //
 const alertBanner = document.getElementById('alert');
@@ -18,4 +18,24 @@ alertBanner.addEventListener('click', e => {
     if (element.className === 'alert-banner-close') {
         alertBanner.style.display = 'none';
     }   
+});
+
+////////////// Messaging Section ///////////////
+
+// Variables //
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+// Event Listener for send button
+send.addEventListener('click', () => {
+    if (user.value === '' && message.value === '') {
+        alert('Please fill out user and message fields before sending');
+    } else if (user.value === '') {
+        alert('Please fill out the user field before sending');
+    } else if (message.value === '') {
+        alert('Please fill out the message field before sending');
+    } else {
+        alert('Message successfully sent to: ${user.value}');
+    }
 });
